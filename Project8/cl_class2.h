d1df2e64ecfec87587e3b085ebe314ada0ca5852
Елизавета Куличенко <Elicoolich@yandex.ru>
2020-05-30 19:05:05 +0300

    косметические изменения кода

--- Project8/cl_class2.h
+++ Project8/cl_class2.h
@@ -2,14 +2,12 @@
 #ifndef CL_CLASS2 //см. 5.7 методичку
 #define CL_CLASS2
 #include "cl_base.h"
-#include<string>
+#include <string>
 class cl_class2 :public cl_base
 {
 public:
     cl_class2(std::string name, int state) : cl_base::cl_base(name, nullptr, state) {}
     int const get_class() override { return 2; }
 };
-
-
 
 #endif
